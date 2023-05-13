7e0fe4f792b1477d9b4a905a724f68bfadbd4d1f
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-05-13 11:43:35 +0530

    Made Few changes

--- Source Package/objects/Inventory.java
+++ Source Package/objects/Inventory.java
@@ -14,7 +14,7 @@ public class Inventory{
         this.inventory.add(item);
     }
 
-    // Check whether items contains in the inventoru
+    // Check whether items contains in the inventoy
 
     // Removing Item from the inventory
     public Item remInvent(Item item){
