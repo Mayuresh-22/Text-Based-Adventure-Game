d811e77785c91fe202edfa405e7162fd2f32a7e7
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-05-13 11:54:53 +0530

    Made Few changes

--- Source Package/game/Game.java
+++ Source Package/game/Game.java
@@ -173,9 +173,7 @@ public class Game implements Serializable{
                     }
                 }
                 else if(item == items_no[items_no.length-1]){
-                    else{
-                        System.out.println(UserInterface.WHITE+"There is no \""+getLocationRoomItem(item).getName()+"\" to collect.");
-                    }
+                    System.out.println(UserInterface.WHITE+"There is no \""+getLocationRoomItem(item).getName()+"\" to collect.");
                 }
             }
         }
