71ca3bf95c986ccd3efe1cd189eeee8e83fac557
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-05-13 10:58:15 +0530

    Made Few changes

--- Source Package/game/Game.java
+++ Source Package/game/Game.java
@@ -39,7 +39,7 @@ public class Game implements Serializable{
 
         /* Creating Game rooms */
         // 0
-        map.add(new Room("Forest", GREEN+"The forest before you is a vast and ancient realm,\na place where the trees loom like giants and the very air is thick with magic and mystery.\nBut for those brave enough to explore its winding paths and hidden glades,\nthe forest is a place of wonder and enchantment,\na realm where anything is possible.", [0, 1], 1, 2, 4, -1, 500));
+        map.add(new Room("Forest", GREEN+"The forest before you is a vast and ancient realm,\na place where the trees loom like giants and the very air is thick with magic and mystery.\nBut for those brave enough to explore its winding paths and hidden glades,\nthe forest is a place of wonder and enchantment,\na realm where anything is possible.", , 1, 2, 4, -1, 500));
         //1
         map.add(new Room("Dead end", RED+"You come to a dead end.\nThe path ends abruptly, with no sign of a way forward.\nThe forest around you seems to close in, the trees pressing in from all sides.\nThe dead end seems to be more than just a simple obstacle; it feels like a warning,\na sign that you have wandered into a place where you do not belong.", -1, 0, -1, -1, -30));
         //2
