7221558c8880be16d87c72f9388858aad5fae8d2
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-05-13 10:25:51 +0530

    Made Few changes

--- Source Package/game/LoadGame.java
+++ Source Package/game/LoadGame.java
@@ -4,6 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
+/*
+ * 1. The LoadGame class is used to load the game state from the save file. 
+ * The save file is a binary file that contains the game state. 
+ * The save file is created by the SaveGame class. The Game class is a Serializable class. 
+ * Therefore, we can use the ObjectInputStream class to read the game state from the save file.
+2. In the loadGame method, we create a File object. 
+If the save file exists, we create a FileInputStream object and an ObjectInputStream object. 
+Then, we read the game state from the save file. We return the game state.
+ */
+
 public class LoadGame {
     public Game loadGame(){
         File f = new File("save.txt");
